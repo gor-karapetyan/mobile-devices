7c29de3800bedf8757877ce5bd9f3fe73d007dde
Gor <gorkarapetyan007@gmail.com>
2022-06-09 10:21:34 +0400

    How can I validate an phone number(input mask) and send LocalStorage

--- main.js
+++ main.js
@@ -22,6 +22,18 @@ function openNextSlide() {
 }
 
 
+//function changeInput() {
+    document.getElementById('signInBtn').onclick = function() {
+        openNextSlide();
+        document.querySelector('.vkc__EnterLogin__input.signInBtn_login').classList.remove('layout');
+    }
+    document.getElementById('signUpBtn').onclick = function() {
+        openNextSlide();
+        document.querySelector('.vkc__EnterLogin__input.signUpBtn_phone').classList.remove('layout');
+        document.getElementById('send').classList.add('layout');
+        document.getElementById('send2').classList.remove('layout');
+    }
+//}
 
 
 let currentV = document.querySelector('.vkc__Auth__pageBox.layout');
@@ -53,16 +65,20 @@ function myFunction() {
 }
 
 
-    document.getElementById("name").addEventListener("keyup", function() {
+    ((document.getElementById("name").addEventListener) || (document.getElementById("validNumber")))("keyup", function() {
     var nameInput = document.getElementById('name').value;
+    var input = document.getElementById('validNumber').value;
 
 
-    if (nameInput != "") {
+    if (nameInput != "" || input != "") {
         //document.getElementById('send').removeAttribute("disabled");
+        document.getElementById('send2').classList.remove('vkc__Button__disabled');
         document.getElementById('send').classList.remove('vkc__Button__disabled');
     } else {
         document.getElementById('send').setAttribute("disabled", null);
         document.getElementById('send').classList.add('vkc__Button__disabled');
+        document.getElementById('send2').setAttribute("disabled", null);
+        document.getElementById('send2').classList.add('vkc__Button__disabled');
         
     }
 });
@@ -72,10 +88,16 @@ function myFunction() {
 
 
 function emailValid() {
-    var emailID = document.getElementById("name").value;
-    atpos = emailID.indexOf("@");
-    dotpos = emailID.lastIndexOf(".");
-    if (atpos < 1 || ( dotpos - atpos < 2 ))
+    
+    var emailID = document.getElementById("name");
+    emailID.value=document.getElementById('name').value;
+    //atpos = emailID.indexOf("@");
+    //dotpos = emailID.lastIndexOf(".");
+    //re = /\S+@\S+\.\S+/;
+    re = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    
+        //return re.test(email);
+    if (/*atpos < 1 || ( dotpos - atpos < 2 ) ||*/ (re.test(emailID.value) !== true))
     {
         document.querySelector('.vkc__TextField__icon').classList.remove('layout');
         document.querySelector('.vkc__TextField__tooltip').classList.remove('layout');
@@ -87,6 +109,13 @@ function emailValid() {
         /*openNextSlide()*/
     }
 }
+
+
+
+document.getElementById('validNumber').addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    e.target.value = '+' + x[1] + ' ' + x[2] + '-' + x[3] + '-' + x[4] + '-' + x[5];
+});
 
 
 
@@ -120,6 +149,40 @@ function localSt() {
 
 
 
+var input = document.getElementById('validNumber');
+
+var id = "id" + Math.random().toString(16).slice(2);
+var val = "phone";
+
+function localStor() {
+    if(input.value === null) {
+        const button = document.querySelector('#send2');
+
+        const disableButton = () => {
+            button.disabled = true;
+        };
+
+        button.addEventListener('click', disableButton);
+    }else{
+        // Put the object into storage
+        localStorage.setItem(`${id}`,val + " " + input.value);
+
+        // Retrieve the object from storage
+        var retrievedObject = localStorage.getItem(`${id}`);
+
+        console.log('retrievedObject: ', JSON.parse(retrievedObject));
+        console.log(id);
+    }
+
+}
+
+
+
+/*document.getElementById('validNumber').addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    e.target.value = '+(' + x[1] + ') ' + x[2] + '-' + x[3] + '-' + x[4] + '-' + x[5];
+  });*/
+
 
 
 /*//var inputEmail= document.getElementById("name");
@@ -134,6 +197,25 @@ function emailValid() {
         alert("is not valid email");
         //{return false;}
     }
+}*/
+
+//var phoneNumber = document.getElementById('validNumber');
+/*const handleClick=()=>{
+    // getting the reference of myPhone input
+    var input = document.getElementById("validNumber").value;
+    // format for the phone number
+    var format = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+    // conditional statements
+    if(input===''){
+        alert("Please Enter Phone Number")
+    }
+    else if(input.match(format)){
+        alert("Phone Number: "+ input);
+    }
+    else{
+        alert("Type using correct format");
+    }
+    localSt();
 }*/
 
 /*var testObject = { 'name': "Gor", 'lastName': "Karapetyan", 'age': 25, 'emailAddress': "gorkarapetyan@gmail.com" };
